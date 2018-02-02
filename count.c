521be75ef5b0a9c313a3c5316a7910d6e8877486
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-02-02 16:18:31 +0530

    count.c
    count number of degits

---
+++ count.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+long n, temp, d, sum = 0;
+printf("Enter the number"); 
+scanf("%ld", &n);
+temp = n;
+while (n > 0)
+{
+d = n % 10;
+sum  = sum + d;
+n /= 10;
+}
+printf("Given number = %ld\n", temp);
+printf("Sum of the digits %ld = %ld\n", temp, sum);
+}
