c3b52e02f5d5e5998bbdf9ab4253a0d01fa95b95
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-02-09 16:08:12 +0530

    2\9\timesubtra.c

---
+++ 2\9\timesubtra.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int time,mins,sub;
+printf("enter the time and mins:",time,mins);
+time=time/24;
+mins=time%60;
+sub=time-mins/24-60;
+printf("same format of time and minutes:,sub);
+getch();
+}
