064088fe41347418d9378c18b90355f3b0acec29
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-02-09 15:59:40 +0530

    Update 2\9\arrayofn.c

--- 2\9\arrayofn.c
+++ 2\9\arrayofn.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main(void) 
+int main() 
 {
-	int arr[10],i,n;
-	printf("enter the size of the array:");
-	scanf("%d",&n);
-	printf("\n enter the array elments:");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-		printf("\n%d %d",arr[i],i);
-	}
-	return 0;
+int arr[10],i,n;
+printf("enter the size of the array:");
+scanf("%d",&n);
+printf("\n enter the array elments:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+printf("\n%d %d",arr[i],i);
+}
+return 0;
 }
