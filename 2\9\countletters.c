49d1b89428abdc78d75163327a62aa9e3d91e65f
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-02-09 16:24:18 +0530

    2\9\countletters.c

---
+++ 2\9\countletters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int i,c=0;
+char a[50];
+ gets(a);
+for(i=0;a[i]!=0;i++)
+{
+if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='z')
+c++;
+}
+ printf("%d",c);
+ return 0;
+}
