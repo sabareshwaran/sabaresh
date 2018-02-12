e5a1b2f3884d8e76f318b74cf2ddcba42a22a927
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-02-12 16:27:21 +0530

    12\2\2018\print n times the word.c

---
+++ 12\2\2018\print n times the word.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+char s[100];
+int n,i;
+printf("enter the word");
+scanf("%s",s);
+printf("ebnter a number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n %s",s);
+}
+}
