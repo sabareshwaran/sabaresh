e839d8cdd41aad91901c075a72e8614a393ab97a
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-02-12 15:44:57 +0530

    2\12\countspace.c

---
+++ 2\12\countspace.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,count=0;
+char a[200];
+printf("\n enter the sentence:");
+scanf("%[^\n]s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+count++;
+}
+}
+printf("\n no. of space: %d",count);
+return 0;
+}
