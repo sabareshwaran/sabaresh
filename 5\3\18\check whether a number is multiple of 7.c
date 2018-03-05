b79ab68c58fcd4221a24fd91ccf4862cda563717
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-03-05 16:42:44 +0530

    5\3\18\check whether a number is multiple of 7.c

---
+++ 5\3\18\check whether a number is multiple of 7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("enter a numbers:");
+  scanf("%d",&a);
+  if(a%7==0)
+  printf("\n Multiple of 7");
+  else
+  printf("\n Not Multiple of 7"); 
+  return 0;
+}
