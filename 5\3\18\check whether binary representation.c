c181c603842b6e5d71bd88eeed468ba3cc62cc2f
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-03-05 15:43:46 +0530

    5\3\18\check whether binary representation.c

---
+++ 5\3\18\check whether binary representation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main() 
+{
+char a[100];
+int n,i,num,count=0;
+scanf("%s",a);
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i]=='0'||a[i]=='1')
+{   
+count++;
+}
+else
+{
+printf("no..");
+break;
+}
+}
+if(n==count)
+{
+printf("yes");
+}
+getch();
+}
